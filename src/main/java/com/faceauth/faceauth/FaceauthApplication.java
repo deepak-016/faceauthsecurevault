a8327e24fa3c1a5db0d628cdd5320e73fbac4b6c
deepak-016 <demondeepak33@gmail.com>
2026-07-12 14:04:30 +0530

    Update FaceauthApplication.java

--- src/main/java/com/faceauth/faceauth/FaceauthApplication.java
+++ src/main/java/com/faceauth/faceauth/FaceauthApplication.java
@@ -2,8 +2,10 @@ package com.faceauth.faceauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class FaceauthApplication {
 
     public static void main(String[] args) {
